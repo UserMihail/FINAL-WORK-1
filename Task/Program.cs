933855df597ba98b25f53a9f58a28c4f246f09f9
Mihail <horunzhy.m@yandex.ru>
2023-06-17 14:02:27 +0300

    Написали задачу

---
+++ Task/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вывести слова меньше либо равные трем символам.
+
+Console.Clear();
+
+Console.WriteLine("Enter words: ");
+string? array = Console.ReadLine();
+
+string[] word = array.Split(' '); 
+
+for(int i = 0; i < word.Length; i++)
+{
+    if(word[i].Length < 4)
+    {
+        Console.WriteLine($"[{word[i]}]");
+    }
+
+}    
